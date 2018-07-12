2a28909cbd9dbec8b34db994589f6c8c826aca74
DanikNik <nikolsky.dan@gmail.com>
2018-07-12 18:31:32 +0300

    update

--- MyToken.sol
+++ MyToken.sol
@@ -1,38 +1,71 @@
 pragma solidity ^0.4.24;
 
-contract MyToken{
+contract EnterpriseEcosystem{
+    struct Factory{
+        uint8 _factory_id;
+        address _owner;
 
-    mapping(uint8 => address) public address_book;
-    mapping(uint8 => uint) public token_book;
-    event Transfer(address _from, address _to, uint8 _token_id);
-    event EmitedToken(address _from, uint8 _token_id, uint _token_data);
-    event DestructedToken(address _from, uint8 _token_id);
-    event ChangedToken(address _from, uint8 _token_id, uint _new_token_data);
-
-    function createToken(uint _tokenData, uint8 _token_id) public {
-        uint newTokenData = _tokenData;
-        uint8 newTokenId = _token_id;
-        token_book[newTokenId] = newTokenData;
-        address_book[newTokenId] = msg.sender;
-        emit EmitedToken(msg.sender, _token_id, _tokenData);
+        string _lable;
+        uint256 _default_price_for_product;
+        uint256 _capital;
+    }
+    struct Product{
+        uint8 _product_id;
+        address _owner;
+        uint8 _factory_assigned;
+        string _title;
+        uint256 _price;
     }
 
-    function transfer(address _to, uint8 _token_id) public {
-        require(msg.sender == address_book[_token_id]);
-        address_book[_token_id] = _to;
-        emit Transfer(msg.sender, _to, _token_id);
+    mapping(uint8=>address) factory_own_list;
+    mapping(uint8=>Factory) factory_instance_list;
+
+    mapping(uint8=>address) product_own_list;
+    mapping(uint8=>Product) product_instance_list;
+
+
+    function emit_factory(uint8 _factory_id, string _lable){
+        factory_own_list[_factory_id] = msg.sender;
+        Factory new_factory = new Factory(_factory_id, msg.sender, _lable, 0, 0);
+        factory_instance_list[_factory_id] = new_factory
     }
 
-    function destructToken(uint8 _token_id) public {
-        require(address_book[_token_id] == msg.sender);
-        address_book[_token_id] = 0;
-        token_book[_token_id] = 0;
-        emit DestructedToken(msg.sender, _token_id);
+    function transfer_factory(uint8 _factory_id, address _to){
+        require(factory_own_list[_factory_id] == msg.sender);
+        factory_own_list[_factory_id] = _to;
+        //Money charging algorythm
+    }
+    function change_factory_data(uint8 _factory_id, string _lable){
+        factory_instance_list[_factory_id]._lable = _lable;
+    }
+    function change_factory_data(uint8 _factory_id, uint256 _default_price_for_product){
+        factory_instance_list[_factory_id]._default_price_for_product = _default_price_for_product;
+    }
+    function destruct_factory(uint8 _factory_id){
+        delete factory_own_list[_factory_id];
+        delete factory_instance_list
     }
 
-    function setData(uint8 _token_id, uint _new_data) public {
-        require(address_book[_token_id] == msg.sender);
-        token_book[_token_id] = _new_data;
-        emit ChangedToken(msg.sender, _token_id, _new_data);
+    function emit_product(uint8 _product_id, uint8 _factory_id, string _title){
+      if (factory_own_list[_factory_id] == msg.sender){
+        Product new_product = new Product(_product_id, mag.sender, _factory_id, _title, 0); //TODO: владелец фабрики эмитит токен в свой кошелек или как? Я забыл, спросить у Леши
+      } else {
+        Product new_product = new Product(_product_id, mag.sender, _factory_id, _title, factory_own_list[_factory_id]._default_price_for_product);
+        // Механизм списания денег
+      }
+      product_own_list[_product_id] = msg.sender;
+      product_instance_list[_product_id] = new_product;
     }
+    function transfer_product(uint8 _product_id, address _to){
+        require(product_own_list[_product_id] == msg.sender);
+        product_own_list[_product_id] = _to;
+    }
+    function change_product_data(uint8 _product_id, uint256 _price){
+      require(product_own_list[_product_id] == msg.sender);
+      product_instance_list[_product_id]._price = _price;
+    }
+    function destruct_product(uint8 _product_id){
+      delete product_own_list[_product_id];
+    }
+
 }
